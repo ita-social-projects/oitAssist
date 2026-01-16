7377d21ecd346a56d0e133fcbaff796a61cbf96d
Olexandr <Mega.alex.alex2000@gmail.com>
2026-01-16 22:09:16 +0200

    initial commit

---
+++ src/main/java/com/itasocialacademy/oitassist/OitAssistApplication.java
@@ -0,0 +1,11 @@
+package com.itasocialacademy.oitassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OitAssistApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OitAssistApplication.class, args);
+    }
+}
---
+++ src/test/java/com/itasocialacademy/oitassist/OitAssistApplicationTests.java
@@ -0,0 +1,8 @@
+package com.itasocialacademy.oitassist;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OitAssistApplicationTests {
+}
