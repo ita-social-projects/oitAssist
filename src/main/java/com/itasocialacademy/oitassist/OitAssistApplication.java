64e04550c47d4f7c03a1f209e99e1cf82ba58cfc
Olexandr Pohranychnyi <78477927+SashaPog@users.noreply.github.com>
2026-02-13 14:34:49 +0200

    Feature/34/configure swagger (#96)

--- src/main/java/com/itasocialacademy/oitassist/OitAssistApplication.java
+++ src/main/java/com/itasocialacademy/oitassist/OitAssistApplication.java
@@ -2,10 +2,12 @@ package com.itasocialacademy.oitassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.modulith.Modulith;
 
 @Modulith
 @SpringBootApplication
+@ConfigurationPropertiesScan()
 public class OitAssistApplication {
     public static void main(String[] args) {
         SpringApplication.run(OitAssistApplication.class, args);
