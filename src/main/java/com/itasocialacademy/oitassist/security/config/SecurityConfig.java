4c7b76f9270fa100247ef4cad4063391e36cbbd2
SaenkoVova <vova080520@gmail.com>
2026-02-27 11:11:50 +0200

    Task #25 fix request matchers

--- src/main/java/com/itasocialacademy/oitassist/security/config/SecurityConfig.java
+++ src/main/java/com/itasocialacademy/oitassist/security/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST,
                     "/api/v1/registration",
                     "/api/v1/registration/resend-email")
+                .permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/news/{id}")
                 .permitAll()
                 .requestMatchers("/api", "/api/**")
