9db2e0f73cfe4ee38c01c1e481636dfe5c2d7a0a
MarynaHuz <maryna.huz.dev@gmail.com>
2026-03-25 14:10:54 +0200

    style(filemanager): fix formatting in `package-info.java`

--- src/main/java/com/itasocialacademy/oitassist/filemanager/package-info.java
+++ src/main/java/com/itasocialacademy/oitassist/filemanager/package-info.java
@@ -1,18 +1,18 @@
 @ApplicationModule(
-        displayName = "File Manager",
-        allowedDependencies = {
-                "core",
-                "core::RestController",
-                "core::AbstractRestControllerImpl",
-                "core::BaseService",
-                "core::AbstractServiceImpl",
-                "core::GeneralMapper",
-                "core::EntityDTO",
-                "core::CreateEntityDTO",
-                "core::UpdateEntityDTO",
-                "core::LongEntity",
-                "core::EntityRepository",
-        })
+    displayName = "File Manager",
+    allowedDependencies = {
+        "core",
+        "core::RestController",
+        "core::AbstractRestControllerImpl",
+        "core::BaseService",
+        "core::AbstractServiceImpl",
+        "core::GeneralMapper",
+        "core::EntityDTO",
+        "core::CreateEntityDTO",
+        "core::UpdateEntityDTO",
+        "core::LongEntity",
+        "core::EntityRepository",
+    })
 package com.itasocialacademy.oitassist.filemanager;
 
 import org.springframework.modulith.ApplicationModule;
