6cc57c20caf2ff058db4fd43bb9f5337e023ecc7
MarynaHuz <maryna.huz.dev@gmail.com>
2026-04-01 22:17:37 +0300

    feat(filemanager): add FileUploadException and error code for file upload failure
    Introduce `FileUploadException` to handle errors during file uploads with descriptive messages and a cause. Add `FILE_UPLOAD_FAILED` to the `ErrorCode` enum for standardized error categorization and improved error handling.

--- src/main/java/com/itasocialacademy/oitassist/core/enums/ErrorCode.java
+++ src/main/java/com/itasocialacademy/oitassist/core/enums/ErrorCode.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * Represents application-specific error codes used across all layers.
+ *
  * <p>
  * Each {@code ErrorCode} is associated with an {@link ErrorCategory}, which
  * determines the type of error and is used for mapping to HTTP status codes in
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
     COMMON_INTERNAL_ERROR(ErrorCategory.TECHNICAL),
+    FILE_UPLOAD_FAILED(ErrorCategory.TECHNICAL),
     COMMON_VALIDATION_FAILED(ErrorCategory.VALIDATION),
 
     TOKEN_EXPIRE(ErrorCategory.AUTHENTICATION),
