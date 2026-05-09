cef588253cd024a200f0b794b128abf635e542c9
MarynaHuz <maryna.huz.dev@gmail.com>
2026-05-09 09:25:58 +0300

    refactor(auth): remove unused test classes

--- src/test/java/com/itasocialacademy/oitassist/auth/controller/RegistrationControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.itasocialacademy.oitassist.auth.controller;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("Unit tests for Registration Controller")
-class RegistrationControllerTest {
-}
--- src/test/java/com/itasocialacademy/oitassist/auth/controller/UserActivationControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.itasocialacademy.oitassist.auth.controller;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("Unit tests for User Activation Controller")
-class UserActivationControllerTest {
-}
