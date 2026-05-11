9eb0c3f6329328685337c6ad28e4a8de1a6abc48
MarynaHuz <maryna.huz.dev@gmail.com>
2026-05-11 16:34:37 +0300

    feat(security): add microsoft OAuth configuration and callback path

--- src/main/java/com/itasocialacademy/oitassist/core/properties/WebClientProperties.java
+++ src/main/java/com/itasocialacademy/oitassist/core/properties/WebClientProperties.java
@@ -8,5 +8,6 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "web-client")
 @Validated
 public record WebClientProperties(
-    @NotBlank @Pattern(regexp = ".*[^/]$", message = "must not end with '/'") String origin) {
+    @NotBlank @Pattern(regexp = ".*[^/]$", message = "must not end with '/'") String origin,
+    @NotBlank @Pattern(regexp = "^/.*", message = "must start with '/'") String oauthCallbackPath) {
 }
