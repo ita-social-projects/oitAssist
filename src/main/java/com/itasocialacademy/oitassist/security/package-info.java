a233271935524414edc82e7ba8b523a3a9cc5c25
MarynaHuz <maryna.huz.dev@gmail.com>
2026-05-11 08:12:59 +0300

    refactor(security): update allowedDependencies with UserAuthDetails and Role

--- src/main/java/com/itasocialacademy/oitassist/security/package-info.java
+++ src/main/java/com/itasocialacademy/oitassist/security/package-info.java
@@ -3,7 +3,9 @@
         "core",
         "core::GlobalExceptionHandler",
         "core::AuthenticationException",
-        "user::UserFacade"})
+        "user::UserFacade",
+        "user :: UserAuthDetails",
+        "user :: Role"})
 package com.itasocialacademy.oitassist.security;
 
 import org.springframework.modulith.ApplicationModule;
