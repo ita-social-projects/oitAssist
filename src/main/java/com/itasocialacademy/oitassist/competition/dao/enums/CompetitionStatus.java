397f7897b1413ea0d5395d92cf5142b1378d10ff
AdamT3H <adamogorodnik@gmail.com>
2026-05-12 14:04:59 +0300

    feat: update CompetitionStatus enum

--- src/main/java/com/itasocialacademy/oitassist/competition/dao/enums/CompetitionStatus.java
+++ src/main/java/com/itasocialacademy/oitassist/competition/dao/enums/CompetitionStatus.java
@@ -1,5 +1,8 @@
 package com.itasocialacademy.oitassist.competition.dao.enums;
 
+import org.springframework.modulith.NamedInterface;
+
+@NamedInterface("CompetitionStatus")
 public enum CompetitionStatus {
     INCOMING, INPROGRESS, FINISED, ARCHIVED
 }
