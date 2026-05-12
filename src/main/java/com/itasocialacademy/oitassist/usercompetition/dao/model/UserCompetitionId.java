15189c02bafc66920587f34a3814511301d828ae
AdamT3H <adamogorodnik@gmail.com>
2026-05-12 15:26:37 +0300

    Fix(UserConpetitionAdd): @Getter annotation for JPA field access.:

--- src/main/java/com/itasocialacademy/oitassist/usercompetition/dao/model/UserCompetitionId.java
+++ src/main/java/com/itasocialacademy/oitassist/usercompetition/dao/model/UserCompetitionId.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.io.Serializable;
 
 @Embeddable
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
