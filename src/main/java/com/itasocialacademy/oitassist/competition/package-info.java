a2b2f9381831f1d19b9316c451f34f95a6e3e294
Anton Pampukha <antonpampuha4@gmail.com>
2026-06-12 14:39:49 +0300

    feat: add JpaAuditingConfig for automatically setting/updating createdBy, createdAt, updatedBy, updatedAt fields

--- src/main/java/com/itasocialacademy/oitassist/competition/package-info.java
+++ src/main/java/com/itasocialacademy/oitassist/competition/package-info.java
@@ -1,5 +1,4 @@
-@ApplicationModule(allowedDependencies = {
-})
+@ApplicationModule(allowedDependencies = {"security::SecurityFacade"})
 package com.itasocialacademy.oitassist.competition;
 
 import org.springframework.modulith.ApplicationModule;
