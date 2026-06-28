2d8ff8e42ac3350b2e9961189b0cc2b611dd9345
Ihor Selianyk <iseljani@gmail.com>
2026-06-28 16:19:29 +0300

    feat(filemanager): add supports(entityType, role) to FilePolicy

--- src/main/java/com/itasocialacademy/oitassist/filemanager/validation/interfaces/FilePolicy.java
+++ src/main/java/com/itasocialacademy/oitassist/filemanager/validation/interfaces/FilePolicy.java
@@ -1,11 +1,23 @@
 package com.itasocialacademy.oitassist.filemanager.validation.interfaces;
 
+import com.itasocialacademy.oitassist.filemanager.dao.enums.FileRole;
+import com.itasocialacademy.oitassist.filemanager.dao.enums.RelatedEntityType;
 import com.itasocialacademy.oitassist.filemanager.validation.enums.AllowedExtension;
 import java.util.Optional;
 import java.util.Set;
 import org.springframework.util.unit.DataSize;
 
 public interface FilePolicy {
+    /**
+     * Indicates whether this policy's rules apply to the given combination
+     * of entity type and file role.
+     *
+     * @param entityType the related entity type of the upload
+     * @param role       the role the uploaded file plays for that entity
+     * @return {@code true} if this policy applies
+     */
+    boolean supports(RelatedEntityType entityType, FileRole role);
+
     /**
      * Returns the set of permitted file extensions for this policy.
      *
