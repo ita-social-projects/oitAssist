3bad55a41efd3ba0893bec2615f30c56407a27ad
Ihor Selianyk <iseljani@gmail.com>
2026-06-29 12:27:57 +0300

    refactor(filemanager): changed the AllArgsConstructor's access to private, for correct default behavior

--- src/main/java/com/itasocialacademy/oitassist/filemanager/dto/request/FileUploadRequestDto.java
+++ src/main/java/com/itasocialacademy/oitassist/filemanager/dto/request/FileUploadRequestDto.java
@@ -10,7 +10,7 @@ import lombok.*;
 @Setter
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class FileUploadRequestDto implements CreateEntityDTO<Long> {
     @NotNull
     private RelatedEntityType relatedEntityType;
