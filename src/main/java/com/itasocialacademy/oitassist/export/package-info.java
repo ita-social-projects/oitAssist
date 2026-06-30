3f2c244f289feea237b1b1991b7db95a8943e2ee
Solomiia Bobiak <mia.bobyak@gmail.com>
2026-06-30 15:05:46 +0300

    feat(export): add export controller and module definition

---
+++ src/main/java/com/itasocialacademy/oitassist/export/package-info.java
@@ -0,0 +1,8 @@
+@ApplicationModule(
+    displayName = "Export",
+    allowedDependencies = {
+        "core"
+    })
+package com.itasocialacademy.oitassist.export;
+
+import org.springframework.modulith.ApplicationModule;
