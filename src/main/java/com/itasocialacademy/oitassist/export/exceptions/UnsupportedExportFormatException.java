f4001fc19ad7d45a9de4d621a779c2205f70fc72
Solomiia Bobiak <mia.bobyak@gmail.com>
2026-06-30 15:54:47 +0300

    fix(export): map unsupported format to 400

--- src/main/java/com/itasocialacademy/oitassist/export/exceptions/UnsupportedExportFormatException.java
+++ src/main/java/com/itasocialacademy/oitassist/export/exceptions/UnsupportedExportFormatException.java
@@ -1,9 +1,9 @@
 package com.itasocialacademy.oitassist.export.exceptions;
 
 import com.itasocialacademy.oitassist.core.enums.ErrorCode;
-import com.itasocialacademy.oitassist.core.exceptions.BadRequestException;
+import com.itasocialacademy.oitassist.core.exceptions.BusinessException;
 
-public class UnsupportedExportFormatException extends BadRequestException {
+public class UnsupportedExportFormatException extends BusinessException {
     public UnsupportedExportFormatException(String message) {
         super(message, ErrorCode.COMMON_VALIDATION_FAILED);
     }
