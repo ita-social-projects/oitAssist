30e5e71bda871aade8d2c4cfbf00a08f1bafc6c1
Ihor Selianyk <iseljani@gmail.com>
2026-07-04 18:42:11 +0300

    feat(task): add TaskBody entity

---
+++ src/main/java/com/itasocialacademy/oitassist/task/dao/model/TaskBody.java
@@ -0,0 +1,46 @@
+package com.itasocialacademy.oitassist.task.dao.model;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "task_bodies")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskBody {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false, nullable = false)
+    private Long createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private Long updatedBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
