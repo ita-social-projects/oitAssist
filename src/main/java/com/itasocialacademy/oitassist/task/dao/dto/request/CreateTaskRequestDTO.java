baa6d64f7193e5b7fcd37895c0722cc4353a3a53
Ihor Selianyk <iseljani@gmail.com>
2026-07-04 19:20:41 +0300

    feat(task): add documentation to OpenAPI

--- src/main/java/com/itasocialacademy/oitassist/task/dao/dto/request/CreateTaskRequestDTO.java
+++ src/main/java/com/itasocialacademy/oitassist/task/dao/dto/request/CreateTaskRequestDTO.java
@@ -1,11 +1,32 @@
 package com.itasocialacademy.oitassist.task.dao.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
+@Schema(description = "DTO for for creating a new task")
 public record CreateTaskRequestDTO(
+    @Schema(
+        description = "Title of the task",
+        example = "PowerPoint Різдвяна зірка",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotBlank String title,
+
+    @Schema(
+        description = "Optional description of the task",
+        example = "Cтворити у файлі-розв’язку на одному слайді ...",
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED
+    )
     String description,
-    List<Long> fileIds
+
+    @Schema(
+        description = "IDs of files already uploaded (in a temporary, unattached state) via the file management "
+            + "module, to be attached to this task once it's created",
+        example = "[51, 52]",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
+    @NotEmpty(message = "At least one file must be provided") List<Long> fileIds
 ) {
 }
--- src/main/java/com/itasocialacademy/oitassist/task/dao/dto/response/TaskResponseDTO.java
+++ src/main/java/com/itasocialacademy/oitassist/task/dao/dto/response/TaskResponseDTO.java
@@ -1,11 +1,27 @@
 package com.itasocialacademy.oitassist.task.dao.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(description = "DTO representing a Task entity response")
 @Builder
 public record TaskResponseDTO(
+    @Schema(
+        description = "Unique identifier of the task",
+        example = "3"
+    )
     Long id,
+
+    @Schema(
+        description = "Title of the task",
+        example = "PowerPoint Різдвяна зірка"
+    )
     String title,
+
+    @Schema(
+        description = "Description of the task",
+        example = "Cтворити у файлі-розв’язку на одному слайді ..."
+    )
     String description
-){
+) {
 }
