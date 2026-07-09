57d4623ffe4f18fb77dfd3799ffd6a0c7a46717d
Ihor Selianyk <iseljani@gmail.com>
2026-07-09 15:44:33 +0300

    feat(task): add UpdateTaskRequestDTO with documentation

---
+++ src/main/java/com/itasocialacademy/oitassist/task/dto/request/UpdateTaskRequestDTO.java
@@ -0,0 +1,26 @@
+package com.itasocialacademy.oitassist.task.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import java.util.List;
+
+@Schema(description = "DTO for updating an already existing task")
+public record UpdateTaskRequestDTO(
+    @Schema(
+        description = "Updated title of the task",
+        example = "Оновлена назва завдання",
+        requiredMode = Schema.RequiredMode.REQUIRED) @NotBlank String title,
+    @Schema(
+        description = "Updated description of the task",
+        example = "Оновлений опис завдання",
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED) String description,
+    @Schema(
+        description = "New and already attached ids of files",
+        example = "[51,62]",
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED) List<Long> fileIds,
+    @Schema(
+        description = "File ids to be detached from task",
+        example = "[52]",
+        requiredMode = Schema.RequiredMode.NOT_REQUIRED) List<Long> removedFileIds
+) {
+}
