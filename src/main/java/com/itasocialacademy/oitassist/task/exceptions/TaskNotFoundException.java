cfcb6a22f3cae369ebc47ef4fa978112be01b08a
Ihor Selianyk <iseljani@gmail.com>
2026-07-13 15:30:45 +0300

    refactor(task): update the exception message

--- src/main/java/com/itasocialacademy/oitassist/task/exceptions/TaskNotFoundException.java
+++ src/main/java/com/itasocialacademy/oitassist/task/exceptions/TaskNotFoundException.java
@@ -6,7 +6,7 @@ import com.itasocialacademy.oitassist.core.exceptions.NotFoundException;
 public class TaskNotFoundException extends NotFoundException {
     public TaskNotFoundException(Long taskId) {
         super(
-            "Task with id %s not found".formatted(taskId),
+            "Task with id %s was not found".formatted(taskId),
             ErrorCode.TASK_NOT_FOUND);
     }
 }
