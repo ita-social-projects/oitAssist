9c4c44a267ca00d0eefaf3d1a824e55c0477d453
Solomiia Bobiak <mia.bobyak@gmail.com>
2026-07-13 13:32:31 +0300

    refactor(core): remove generic core/rest base layer
    Delete the generic CRUD base layer (AbstractServiceImpl, AbstractRestControllerImpl, base entities, DTOs, mappers and repository)

--- src/main/java/com/itasocialacademy/oitassist/core/rest/mapper/GeneralMapper.java
+++
@@ -1,8 +0,0 @@
-package com.itasocialacademy.oitassist.core.rest.mapper;
-
-import org.springframework.modulith.NamedInterface;
-
-@NamedInterface("GeneralMapper")
-public interface GeneralMapper<E, C, U, D>
-    extends DtoMapper<E, D>, CreateMapper<E, C>, UpdateMapper<E, U> {
-}
