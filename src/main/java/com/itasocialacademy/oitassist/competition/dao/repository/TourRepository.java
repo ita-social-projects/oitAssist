12983463178767ea73bb938950d0f8edf15bf3da
Anton Pampukha <antonpampuha4@gmail.com>
2026-07-16 22:16:35 +0300

    feat(tour): add method to repository

--- src/main/java/com/itasocialacademy/oitassist/competition/dao/repository/TourRepository.java
+++ src/main/java/com/itasocialacademy/oitassist/competition/dao/repository/TourRepository.java
@@ -4,6 +4,8 @@ import com.itasocialacademy.oitassist.competition.dao.model.Tour;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -23,4 +25,14 @@ public interface TourRepository extends JpaRepository<Tour, Long> {
     List<Tour> findAllByStageIdInOrderBySortPositionAsc(List<Long> stageIds);
 
     Optional<Tour> findByStageIdAndSortPosition(Long stageId, Short sortPosition);
+
+    @Query("""
+            SELECT t FROM Tour t
+            WHERE t.stageId = :stageId
+              AND t.sortPosition < :sortPosition
+            ORDER BY t.sortPosition DESC
+        """)
+    Optional<Tour> findFirstPreviousTour(
+        @Param("stageId") Long stageId,
+        @Param("sortPosition") Short sortPosition);
 }
