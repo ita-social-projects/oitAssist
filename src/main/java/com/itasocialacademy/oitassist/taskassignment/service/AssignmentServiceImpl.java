265e733f56e0c06c992cfb1109831d3217b75f6d
Ihor Selianyk <iseljani@gmail.com>
2026-07-20 19:26:45 +0300

    feat: create methods to get all tasks linked to a certain tour and to get assignment by id

--- src/main/java/com/itasocialacademy/oitassist/taskassignment/service/AssignmentServiceImpl.java
+++ src/main/java/com/itasocialacademy/oitassist/taskassignment/service/AssignmentServiceImpl.java
@@ -6,10 +6,13 @@ import com.itasocialacademy.oitassist.taskassignment.dao.repository.TaskAssignme
 import com.itasocialacademy.oitassist.taskassignment.dto.request.CreateTaskAssignmentRequestDTO;
 import com.itasocialacademy.oitassist.taskassignment.dto.response.TaskAssignmentResponseDTO;
 import com.itasocialacademy.oitassist.taskassignment.exceptions.TaskAlreadyAssignedException;
+import com.itasocialacademy.oitassist.taskassignment.exceptions.TaskAssignmentNotFoundException;
 import com.itasocialacademy.oitassist.taskassignment.mapper.TaskAssignmentMapper;
 import com.itasocialacademy.oitassist.taskassignment.service.interfaces.AssignmentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,7 +39,7 @@ public class AssignmentServiceImpl implements AssignmentService {
         taskAssignment.setTourId(tourId);
         taskAssignment.setRequirements(taskAssignmentMapper.toRequirements(request.requirements()));
 
-        if (request.assignmentVisibility() == null) {
+        if (request.visibility() == null) {
             taskAssignment.setVisibility(AssignmentVisibility.HIDDEN);
         }
 
@@ -44,5 +47,28 @@ public class AssignmentServiceImpl implements AssignmentService {
 
         // TODO: use real task title, when the task facade will be introduced
         return taskAssignmentMapper.toResponse(taskAssignmentRepository.save(taskAssignment), "mock title");
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<TaskAssignmentResponseDTO> getAssignmentsByTourId(Pageable pageable, Long tourId) {
+        // TODO: add a check for tour existence by its id
+
+        log.debug("Getting all task assignments for tour {}, page={}, size={}, sort={}", tourId,
+            pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
+
+        // TODO: add a real task title to response via task facade
+        return taskAssignmentRepository.findAllByTourId(tourId, pageable)
+            .map(entity -> taskAssignmentMapper.toResponse(entity, "mock title"));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public TaskAssignmentResponseDTO getTaskAssignmentById(Long taskAssignmentId) {
+        TaskAssignment assignment = taskAssignmentRepository.findById(taskAssignmentId).orElseThrow(
+            () -> new TaskAssignmentNotFoundException(taskAssignmentId));
+
+        log.debug("Get Task Assignment: Id {}", assignment.getId());
+        return taskAssignmentMapper.toResponse(assignment, "mock title");
     }
 }
--- src/main/java/com/itasocialacademy/oitassist/taskassignment/service/interfaces/AssignmentService.java
+++ src/main/java/com/itasocialacademy/oitassist/taskassignment/service/interfaces/AssignmentService.java
@@ -2,7 +2,13 @@ package com.itasocialacademy.oitassist.taskassignment.service.interfaces;
 
 import com.itasocialacademy.oitassist.taskassignment.dto.request.CreateTaskAssignmentRequestDTO;
 import com.itasocialacademy.oitassist.taskassignment.dto.response.TaskAssignmentResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AssignmentService {
     TaskAssignmentResponseDTO assignTask(Long tourId, CreateTaskAssignmentRequestDTO request);
+
+    Page<TaskAssignmentResponseDTO> getAssignmentsByTourId(Pageable pageable, Long tourId);
+
+    TaskAssignmentResponseDTO getTaskAssignmentById(Long taskAssignmentId);
 }
