809102e2839c73b9d178dba9edbe2508a0c23513
Ihor Selianyk <iseljani@gmail.com>
2026-07-20 16:23:25 +0300

    feat: add lombok Builder annotation for TaskAssignmentResponseDTOs

--- src/main/java/com/itasocialacademy/oitassist/taskassignment/dto/response/TaskAssignmentResponseDTO.java
+++ src/main/java/com/itasocialacademy/oitassist/taskassignment/dto/response/TaskAssignmentResponseDTO.java
@@ -2,7 +2,9 @@ package com.itasocialacademy.oitassist.taskassignment.dto.response;
 
 import com.itasocialacademy.oitassist.taskassignment.dao.enums.AssignmentVisibility;
 import com.itasocialacademy.oitassist.taskassignment.dao.model.TaskRequirements;
+import lombok.Builder;
 
+@Builder
 public record TaskAssignmentResponseDTO(
     Long id,
     Long taskBodyId,
