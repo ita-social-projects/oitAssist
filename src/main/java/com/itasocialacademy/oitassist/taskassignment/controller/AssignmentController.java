3fc0378331ac4083922850e69b27f0992467222e
Ihor Selianyk <iseljani@gmail.com>
2026-07-20 19:28:22 +0300

    feat: add get endpoints for assignments linked to a certain tour and assignment by id

--- src/main/java/com/itasocialacademy/oitassist/taskassignment/controller/AssignmentController.java
+++ src/main/java/com/itasocialacademy/oitassist/taskassignment/controller/AssignmentController.java
@@ -1,10 +1,13 @@
 package com.itasocialacademy.oitassist.taskassignment.controller;
 
+import com.itasocialacademy.oitassist.core.dao.dto.response.PageResponse;
 import com.itasocialacademy.oitassist.taskassignment.dto.request.CreateTaskAssignmentRequestDTO;
 import com.itasocialacademy.oitassist.taskassignment.dto.response.TaskAssignmentResponseDTO;
 import com.itasocialacademy.oitassist.taskassignment.service.interfaces.AssignmentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,5 +24,18 @@ public class AssignmentController {
     public ResponseEntity<TaskAssignmentResponseDTO> assignTask(@PathVariable Long tourId,
         @Valid @RequestBody CreateTaskAssignmentRequestDTO request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(assignmentService.assignTask(tourId, request));
+    }
+
+    @GetMapping("/task-assignments/{assignmentId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<TaskAssignmentResponseDTO> getById(@PathVariable Long assignmentId) {
+        return ResponseEntity.ok().body(assignmentService.getTaskAssignmentById(assignmentId));
+    }
+
+    @GetMapping("/tours/{tourId}/task-assignments")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<PageResponse<TaskAssignmentResponseDTO>> getByTour(@PathVariable Long tourId,
+        @PageableDefault(size = 15, sort = "createdAt") Pageable pageable) {
+        return ResponseEntity.ok(PageResponse.from(assignmentService.getAssignmentsByTourId(pageable, tourId)));
     }
 }
