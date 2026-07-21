67cdbd19236636265a0db23c2402c931d3bd0976
Ihor Selianyk <iseljani@gmail.com>
2026-07-21 17:31:16 +0300

    fix: add an upper bound to maxPoints field in UpdateTaskAssignmentRequestDTO

--- src/main/java/com/itasocialacademy/oitassist/taskassignment/dto/request/UpdateTaskAssignmentRequestDTO.java
+++ src/main/java/com/itasocialacademy/oitassist/taskassignment/dto/request/UpdateTaskAssignmentRequestDTO.java
@@ -2,10 +2,11 @@ package com.itasocialacademy.oitassist.taskassignment.dto.request;
 
 import com.itasocialacademy.oitassist.taskassignment.dao.enums.AssignmentVisibility;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
 public record UpdateTaskAssignmentRequestDTO(
     AssignmentVisibility visibility,
-    @Min(1) Integer maxPoints,
+    @Min(1) @Max(120) Integer maxPoints,
     @Valid TaskRequirementsRequestDTO requirements) {
 }
