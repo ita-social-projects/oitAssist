30fbf68628f73c126b3718e37886df67f610c217
Ihor Selianyk <166725875+solenuk@users.noreply.github.com>
2026-07-22 18:45:30 +0300

    [Feature] Implement facade for task module (#417)
    * feat: update the TaskForumContext to be TaskBodyDetail record
    
    * feat: update the mapper to support TaskBodyDetail
    
    * feat: update the TaskForumFacade to TaskBodyFacade
    
    * refactor(chat): leave a todo in chat module for future TaskAssignmentFacade implementation
    
    * refactor: remove redundant files
    
    * refactor: remove Optional from createTask return type
    
    * feat: add a new custom method for TaskBodyRepository to query the titles based on the given taskIds list
    
    * refactor: move the TaskBodyFacade to the root of the api package
    
    * feat: add new logic for TaskBodyDetail retrieval by id and retrieval of task titles based on the list of ids
    
    * feat: rename TaskForumFacadeImpl to TaskBodyFacadeImpl and add a new logic
    
    * refactor(chat): set a new TaskBodyFacade path
    
    * refactor: put a NamedInterface annotation on package-info instead on a class itself
    
    * refactor: add a ParameterObject annotation to PageResponse endpoints for better documentation

--- src/main/java/com/itasocialacademy/oitassist/task/dao/repository/TaskBodyRepository.java
+++ src/main/java/com/itasocialacademy/oitassist/task/dao/repository/TaskBodyRepository.java
@@ -1,13 +1,21 @@
 package com.itasocialacademy.oitassist.task.dao.repository;
 
 import com.itasocialacademy.oitassist.task.dao.model.TaskBody;
+import com.itasocialacademy.oitassist.task.dao.model.TaskTitleView;
 import com.itasocialacademy.oitassist.task.dto.response.TaskResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface TaskBodyRepository extends JpaRepository<TaskBody, Long> {
     Page<TaskResponseDTO> findAllByOwnerId(Long currentUserId, Pageable pageable);
+
+    @Query(value = "SELECT t.id, t.title FROM TaskBody t WHERE t.id IN :ids")
+    List<TaskTitleView> findTitlesByIds(@Param("ids") Collection<Long> taskIds);
 }
