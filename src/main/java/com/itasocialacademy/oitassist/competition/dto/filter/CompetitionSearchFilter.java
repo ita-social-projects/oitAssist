1472538eb8328bcf7288012deedce618e48baea2
Anton Pampukha <139760146+antonpampukha@users.noreply.github.com>
2026-07-28 17:59:28 +0300

    [Feature] implement search for competition hierarchy (#444)
    * chore: add @ParameterObject to document Pageable in OpenAPI
    
    * feat: add search to CompetitionController
    - add specifications for searching to CompetitionSpecification
    - add CompetitionSearchFilter
    - update getAll methods in CompetitionServiceImpl
    
    * feat: add Javadoc
    
    * feat(competition): add unit tests
    
    * chore(competition): update description in getAllVisible()

---
+++ src/main/java/com/itasocialacademy/oitassist/competition/dto/filter/CompetitionSearchFilter.java
@@ -0,0 +1,21 @@
+package com.itasocialacademy.oitassist.competition.dto.filter;
+
+import com.itasocialacademy.oitassist.competition.dao.enums.CompetitionStatus;
+import com.itasocialacademy.oitassist.competition.dto.validation.HasDateRange;
+import com.itasocialacademy.oitassist.competition.dto.validation.ValidDateRange;
+import java.time.ZonedDateTime;
+import java.util.List;
+import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Builder
+@ValidDateRange
+public record CompetitionSearchFilter(
+    String title,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime dateStart,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime dateFinish,
+
+    List<CompetitionStatus> statuses) implements HasDateRange {
+}
