b44aec570b30b748c7f79b4c00d3279deafc98d0
Serhiy Haleliuk <serggalel@gmail.com>
2026-08-20 13:55:30 +0300

    feat(applications): create an assembler and update the repository

--- src/main/java/com/itasocialacademy/oitassist/participation/dao/repository/ApplicationRepository.java
+++ src/main/java/com/itasocialacademy/oitassist/participation/dao/repository/ApplicationRepository.java
@@ -4,6 +4,7 @@ import com.itasocialacademy.oitassist.participation.dao.enums.RequestStatus;
 import com.itasocialacademy.oitassist.participation.dao.model.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
@@ -12,4 +13,6 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
         Long competitionId,
         Long stageId,
         RequestStatus status);
+
+    List<Application> findAllByCompetitionIdAndStageIdAndStatus(Long competitionId, Long stageId, RequestStatus status);
 }
