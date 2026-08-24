5553d1c757fe6d72fa9b0ec99663b3d23f28568c
Ihor Selianyk <iseljani@gmail.com>
2026-08-24 18:43:58 +0300

    feat: add new mapping

--- src/main/java/com/itasocialacademy/oitassist/taskassignment/mapper/TaskAssignmentMapper.java
+++ src/main/java/com/itasocialacademy/oitassist/taskassignment/mapper/TaskAssignmentMapper.java
@@ -1,5 +1,6 @@
 package com.itasocialacademy.oitassist.taskassignment.mapper;
 
+import com.itasocialacademy.oitassist.competition.api.dto.TourDetail;
 import com.itasocialacademy.oitassist.filemanager.api.dto.FileDetailsDTO;
 import com.itasocialacademy.oitassist.taskassignment.api.dto.TaskAssignmentDetailDTO;
 import com.itasocialacademy.oitassist.taskassignment.dao.model.TaskAssignment;
@@ -7,8 +8,10 @@ import com.itasocialacademy.oitassist.taskassignment.dao.model.TaskRequirements;
 import com.itasocialacademy.oitassist.taskassignment.dto.request.CreateTaskAssignmentRequestDTO;
 import com.itasocialacademy.oitassist.taskassignment.dto.request.TaskRequirementsRequestDTO;
 import com.itasocialacademy.oitassist.taskassignment.dto.response.DetailedTaskAssignmentResponseDTO;
+import com.itasocialacademy.oitassist.taskassignment.dto.response.LinkedToursResponseDTO;
 import com.itasocialacademy.oitassist.taskassignment.dto.response.TaskAssignmentResponseDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import java.util.List;
 
@@ -24,4 +27,7 @@ public interface TaskAssignmentMapper {
 
     DetailedTaskAssignmentResponseDTO toDetailedResponse(TaskAssignment entity, String taskTitle,
         String taskDescription, List<FileDetailsDTO> files);
+
+    @Mapping(target = "tourId", source = "id")
+    LinkedToursResponseDTO toLinkedToursResponse(TourDetail tourDetail);
 }
