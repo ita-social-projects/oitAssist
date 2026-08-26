15d0f60e2348667dd1e707f0c431754c531c79cd
Ihor Selianyk <iseljani@gmail.com>
2026-08-26 14:57:30 +0300

    feat: add version field to dtos

--- src/main/java/com/itasocialacademy/oitassist/taskassignment/dto/response/DetailedTaskAssignmentResponseDTO.java
+++ src/main/java/com/itasocialacademy/oitassist/taskassignment/dto/response/DetailedTaskAssignmentResponseDTO.java
@@ -44,5 +44,7 @@ public record DetailedTaskAssignmentResponseDTO(
 
     @Schema(
         description = "Id of the user who created this assignment",
-        example = "3") Long createdBy) {
+        example = "3") Long createdBy,
+
+    @Schema(description = "Optimistic locking version; must be echoed back on updates") Long version) {
 }
