2c184edffa66229d24eb9133ca41933cd30b796b
Ihor Selianyk <iseljani@gmail.com>
2026-08-27 15:25:41 +0300

    fix: remove unused import

--- src/test/java/com/itasocialacademy/oitassist/taskassignment/service/AssignmentServiceTest.java
+++ src/test/java/com/itasocialacademy/oitassist/taskassignment/service/AssignmentServiceTest.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
