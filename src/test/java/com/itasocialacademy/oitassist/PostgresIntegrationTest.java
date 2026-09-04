e71a66685adc39a6ced55c6e0e4012b87e25c2fa
Anton Pampukha <antonpampuha4@gmail.com>
2026-09-04 20:33:50 +0300

    feat: remove @Testcontainers from PostgresIntegrationTest

--- src/test/java/com/itasocialacademy/oitassist/PostgresIntegrationTest.java
+++ src/test/java/com/itasocialacademy/oitassist/PostgresIntegrationTest.java
@@ -5,18 +5,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 @Tag("integration")
 public abstract class PostgresIntegrationTest {
 
-    @Container
     @ServiceConnection
     static final PostgreSQLContainer<?> POSTGRES =
         new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+
+    static {
+        POSTGRES.start();
+    }
 }
