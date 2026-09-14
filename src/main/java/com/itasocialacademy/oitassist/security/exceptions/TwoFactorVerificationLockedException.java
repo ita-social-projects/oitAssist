35cb930ad635bdeef335f3db0c350ded0bf3f66a
Maryna Huz <maryna.huz.dev@gmail.com>
2026-09-15 01:01:26 +0300

    refactor(security/twofactor): extend AuthenticationException for lockout error
      TwoFactorVerificationLockedException now extends AuthenticationException
      rather than BusinessException, since it represents a failed authentication
      attempt (too many bad codes) rather than a generic business rule violation.

--- src/main/java/com/itasocialacademy/oitassist/security/exceptions/TwoFactorVerificationLockedException.java
+++ src/main/java/com/itasocialacademy/oitassist/security/exceptions/TwoFactorVerificationLockedException.java
@@ -1,9 +1,9 @@
 package com.itasocialacademy.oitassist.security.exceptions;
 
 import com.itasocialacademy.oitassist.core.enums.ErrorCode;
-import com.itasocialacademy.oitassist.core.exceptions.BusinessException;
+import com.itasocialacademy.oitassist.core.exceptions.AuthenticationException;
 
-public class TwoFactorVerificationLockedException extends BusinessException {
+public class TwoFactorVerificationLockedException extends AuthenticationException {
     public TwoFactorVerificationLockedException(String message, ErrorCode errorCode) {
         super(message, errorCode);
     }
